88e55f981ddfd2ebe4e4d7c04020a6bcc4f7d11e
rymate1234 <supermarioryan@gmail.com>
2015-05-07 16:53:47 +0100

    remove debug code

--- simple-tasks.js
+++ simple-tasks.js
@@ -258,6 +258,13 @@ if (Meteor.isServer) {
         return Tags.find({owner: this.userId});
     });
 
+    // enable cors
+    // Listen to incoming HTTP requests, can only be used on the server
+    WebApp.connectHandlers.use(function(req, res, next) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        return next();
+    });
+
 }
 
 
